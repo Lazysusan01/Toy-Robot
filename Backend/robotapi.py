595c0d6d4e8ff95f1fd354db1358f581636555d3
Nicomcgill <nicomcgill@gmail.com>
2024-06-20 09:48:22 +1000

    Fixing Docker-compose & Backend imports

--- Backend/robotapi.py
+++ Backend/robotapi.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, request, jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token
-from .models import User, db  # Ensure your User model is accessible here
+
 from .robot import robot
 
 
